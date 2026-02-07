5b37e7f209d32ca60a3f44e36097c4d7aacc132d
JongChan Choi <jong@chan.moe>
2026-02-08 03:59:57 +0900

    chore(hwpkit/automation): remove local backup/tmp artifacts

--- hwpkit/automation/tools/tmp-print-fileclose.cjs
+++
@@ -1,3 +0,0 @@
-﻿const fs = require('fs');
-const p = JSON.parse(fs.readFileSync('src/spec/parametersets.json','utf8'));
-console.log(Object.keys(p).filter(k=>k.toLowerCase().includes('fileclose')).join('\n'));
